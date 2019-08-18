95ab4df3f671189412c6fd50646ad98610f66866
diegoldn <diegolopes@Diegos-iMac.Home>
2019-08-17 22:08:05 -0300

    feat: add new columns and service thearkcrypto podcast. the user will be able to listen podcasts directly from arkhub

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,18 +1,28 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxAudioPlayerModule } from 'ngx-audio-player';
 import { SharedRoutingModule } from './shared-routing.module';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ColumnComponent } from './components/column/column.component';
+import { SafeHtmlPipe } from './pipes/safe-html.pipe';
 
 @NgModule({
     declarations: [
-        ColumnComponent
+        ColumnComponent,
+        SafeHtmlPipe
     ],
     imports: [
         CommonModule,
-        SharedRoutingModule
+        SharedRoutingModule,
+        NgxAudioPlayerModule,
+        DragDropModule
     ],
     exports: [
         ColumnComponent
+    ],
+    providers: [
+        SafeHtmlPipe
     ]
 })
 export class SharedModule { }
