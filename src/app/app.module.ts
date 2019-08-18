899a595d7d56ff66671602828b75e333f56b8dea
diegoldn <diegolopes@Diegos-iMac.Home>
2019-08-17 22:05:31 -0300

    feat: imported ngx audio and material design modules to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxSmartModalModule } from 'ngx-smart-modal';
 import { AppRoutingModule } from './app-routing.module';
+import { NgxAudioPlayerModule } from 'ngx-audio-player';
 import { AppComponent } from './app.component';
 import { SidebarModule } from 'src/shared/sidebar/sidebar.module';
 import { SharedModule } from 'src/shared/shared.module';
@@ -10,6 +12,7 @@ import { MediumService } from '../shared/services/medium.service';
 import { SteemitService } from 'src/shared/services/steemit.service';
 import { UtilsService } from './../shared/services/utils.service';
 import { RedditService } from 'src/shared/services/reddit.service';
+import { TheArkCryptoPodcastService } from 'src/shared/services/thearkcryptopodcast.service';
 import { DashboardComponent } from './../pages/dashboard/dashboard.component';
 
 @NgModule({
@@ -20,16 +23,19 @@ import { DashboardComponent } from './../pages/dashboard/dashboard.component';
   imports: [
       BrowserModule,
       NgxSmartModalModule.forRoot(),
+      MaterialModule,
       SidebarModule,
       SharedModule,
       AppRoutingModule,
-      HttpClientModule
+      HttpClientModule,
+      NgxAudioPlayerModule
   ],
   providers: [
       MediumService,
       SteemitService,
       UtilsService,
-      RedditService
+      RedditService,
+      TheArkCryptoPodcastService,
   ],
   bootstrap: [AppComponent],
 })
