539c1136bcc0e4c58fa8a2b1f60407c7d84ba59a
diegoldn <diegolopes@Diegos-iMac.Home>
2019-08-17 22:04:55 -0300

    refactor: remove empty constructor from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +6,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'ArkHub';
-
-    constructor() { }
 }
